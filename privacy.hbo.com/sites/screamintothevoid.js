ce6b04c5534934f89669117f27ad8b4c79d7fb4a
Alleyhustler <153684384+Alleyhustler@users.noreply.github.com>
2024-09-18 18:08:30 +0200

    Add files via upload

---
+++ privacy.hbo.com/sites/screamintothevoid.js
@@ -0,0 +1,8 @@
+window.WM = window.WM || {};
+WM.ConsentSiteConfig = {
+    cookiedomain: '.screamintothevoid.com',
+    domId: '4cca8968-d8dc-454a-b534-33b1ded104f5',
+    src: 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js',
+    vendor: {
+    }
+}
